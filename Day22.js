199d4b26bbdcd2e8bf6d9f31ce405033051dc567
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-02-10 13:43:19 +0530

    Day 22 left solved

---
+++ Day22.js
@@ -0,0 +1,21 @@
+//find-pivot-index
+
+var pivotIndex = function(nums) {
+    let totalSum = 0
+    for(let i = 0; i < nums.length; i++) {
+        totalSum += nums[i]
+    }
+    
+    let leftSum = 0
+    
+    for (let i = 0; i < nums.length; i++) {
+        let rightSum = totalSum - nums[i] - leftSum
+        if (rightSum === leftSum) {
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1
+};
+
+console.log(pivotIndex([1,7,3,6,5,6]))

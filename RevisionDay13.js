0b747082ec29cddb0a6d0ad65750680e653f7ea1
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-04-29 21:56:47 +0530

    Revision Day 13 base code added

---
+++ RevisionDay13.js
@@ -0,0 +1,20 @@
+var findWords = function (words) {
+  let rowArr = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+  let arr = [];
+  for (let i = 0; i < words.length; i++) {
+    let str = words[i];
+    let flag = true;
+    for (let j = 0; j < rowArr.length; j++) {
+      for (let k = 0; k < str.length; k++) {
+        if (!rowArr[j].includes(str[k])) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) arr.push(str);
+    }
+  }
+  return arr;
+};
+
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));

1d51bb4b12f1cfe2d845e8c6a92b22bb21391946
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-04-30 15:18:08 +0530

    Revision Day 13 done

--- RevisionDay13.js
+++ RevisionDay13.js
@@ -1,20 +1,26 @@
+//Keyboard Row
 var findWords = function (words) {
   let rowArr = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
   let arr = [];
   for (let i = 0; i < words.length; i++) {
-    let str = words[i];
-    let flag = true;
+    let str = words[i].toLowerCase();
     for (let j = 0; j < rowArr.length; j++) {
+      let flag = true;
       for (let k = 0; k < str.length; k++) {
         if (!rowArr[j].includes(str[k])) {
+        //   console.log(str, rowArr[j], str[k]);
           flag = false;
           break;
         }
       }
-      if (flag) arr.push(str);
+      if (flag) arr.push(words[i]);
     }
   }
   return arr;
 };
 
-console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));
+// console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));
+// console.log(findWords(["omk"]));
+console.log(findWords(["adsdf","sfd"]));
+
+

5e074a7a2a9cf3f195fa8a72e5b5f4e7c6447c7f
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-05-21 00:10:31 +0530

    Leetcode290 done

--- Leetcode290.js
+++ Leetcode290.js
@@ -1,12 +1,25 @@
 // word pattern
 
 var wordPattern = function (pattern, s) {
+    if(pattern.length > 1 && s.length > 1 && !/\s/g.test(s)){
+        return false
+    }
+  const getKey = (val, object) => {
+    for (let [key, value] of Object.entries(object)) {
+      if (value === val) {
+        console.log(key);
+        return key;
+      }
+    }
+  };
   let splitArr = s.split(" ");
-  //   return splitArr;
   let obj = {};
   for (let i = 0; i < splitArr.length; i++) {
     if (
-      Object.keys(obj).includes(pattern[i]) && obj[pattern[i]] !== splitArr[i]
+      (Object.keys(obj).includes(pattern[i]) &&
+        obj[pattern[i]] !== splitArr[i]) ||
+      (Object.values(obj).includes(splitArr[i]) &&
+        getKey(splitArr[i], obj) !== pattern[i])
     ) {
       return false;
     } else {
@@ -16,4 +29,9 @@ var wordPattern = function (pattern, s) {
   return true;
 };
 
-console.log(wordPattern("abba", "dog cat cat dog"));
+// console.log(wordPattern("abba", "dog dog dog dog"));
+// console.log(wordPattern("abba", "dog cat cat dog"));
+// console.log(wordPattern("abba", "dog cat cat fish"));
+console.log(wordPattern("a", "a"));
+// console.log(wordPattern("word", "word"));
+// console.log(wordPattern("he", "unit"));

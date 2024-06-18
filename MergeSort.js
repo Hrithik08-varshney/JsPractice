c876395d8a38f7f07d5b86c6185a167a12208611
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-06-19 00:30:28 +0530

    merge sort added and 912 submitted

--- MergeSort.js
+++ MergeSort.js
@@ -1,12 +1,10 @@
-//Sort an Array
-
-var sortArray = function (nums) {
-  if (nums.length <= 1) return nums;
-  let mid = Math.floor(nums.length / 2);
-  let left = sortArray(nums.slice(0, mid));
-  let right = sortArray(nums.slice(mid));
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
   return merge(left, right);
-};
+}
 
 function merge(left, right) {
   let sortedArr = [];
@@ -18,4 +16,4 @@ function merge(left, right) {
   return [...sortedArr, ...left, ...right];
 }
 
-console.log(sortArray([5, 2, 3, 1]));
+console.log(mergeSort([9, 5, 7, 4, 1, 2, 9, 10]));

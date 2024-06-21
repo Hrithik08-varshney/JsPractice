dc1fee9e03071ae6b33132a4c4feae8212d04a8d
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-06-21 22:50:21 +0530

    debounce function

---
+++ Js Interview Syntax/index.js
@@ -0,0 +1,29 @@
+/*  Debouncing */
+
+const getData = () => {
+  //api function
+  console.log("API hit....");
+};
+const debounce = function (fn, delay) {
+  let timer;
+  return function () {
+    let context = this; //The context variable is set to this inside the returned function.
+    //This ensures that when fn is invoked, it is called with the correct context (this value).
+
+    args = arguments; //Inside the setTimeout callback function,
+    //arguments refers to the arguments passed to the debounced function, not the arguments of the setTimeout function itself.
+
+    clearTimeout(timer); //clearing the previous function
+
+    timer = setTimeout(() => {
+      fn.apply(context, arguments); //function will work after 300 milliseconds
+    }, delay);
+  };
+};
+const delayFunction = debounce(getData, 300); //will hit on key press
+
+//The use of arguments inside an arrow function (() => { ... }) 
+//can sometimes lead to unexpected behavior because arrow 
+//functions do not have their own arguments object; they inherit it from the enclosing non-arrow function. 
+//In this case, arguments inside setTimeout(() => { ... }) refers to the arguments of the debounce function,
+// not of the setTimeout function itself.

01ab81d387553b71d6268618404875dfef388896
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-07-05 23:25:09 +0530

    binary search tree added

--- JsInterviewSyntax/BinarySearch/BinarySearch.js
+++ JsInterviewSyntax/BinarySearch/BinarySearch.js
@@ -1,6 +1,17 @@
 const BinarySearch = (arr, elem) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] === elem) {
+      return middle;
+    } else if (arr[middle] < elem) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
+  }
+  return -1;
+};
 
-  };
-  
-  console.log(BinarySearch([4, 3, 12, 56, 7, 5, 43], 43));
-  
+console.log(BinarySearch([-1, 0, 3, 5, 9, 12], 9));

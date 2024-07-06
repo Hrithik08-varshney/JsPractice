7dcce1cac81327e6dabb9c1fb286cbc690f38906
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-07-07 00:01:02 +0530

    leetcode1539 done with different approach

--- Leetcode1539.js
+++ Leetcode1539.js
@@ -1,11 +1,23 @@
+// var findKthPositive = function (arr, k) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] <= k + count) {
+//       count++;
+//     }
+//   }
+//   return k + count;
+// };
+
+//another approach------------------->
+
 var findKthPositive = function (arr, k) {
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] <= k + count) {
-      count++;
+  let newArr = [];
+  for (let num = 1; newArr.length < k; num++) {
+    if (!arr.includes(num)) {
+      newArr.push(num);
     }
   }
-  return k + count;
+  return newArr[k - 1];
 };
 
 console.log(findKthPositive([2, 3, 4, 7, 11], 5));

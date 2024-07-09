a9171030fb24b1171e070281e5cac15cae45186c
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-07-10 00:04:37 +0530

    added with recursion

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,17 +1,26 @@
+//brute force approach -------------------------------------->
+
+// const fibonacii = (n) => {
+//   let n1 = 0,
+//     n2 = 1;
+//   if (n <= 1) {
+//     return n;
+//   } else {
+//     let sum = 0;
+//     for (let i = 1; i < n; i++) {
+//       sum = n1 + n2;
+//       n1 = n2;
+//       n2 = sum;
+//     }
+//     return sum;
+//   }
+// };
+
+//recursion -------------------------------------->
 const fibonacii = (n) => {
-  let n1 = 0,
-    n2 = 1;
-  if (n <= 1) {
-    return n;
-  } else {
-    let sum = 0;
-    for (let i = 1; i < n; i++) {
-      sum = n1 + n2;
-      n1 = n2;
-      n2 = sum;
-    }
-    return sum;
-  }
+  if (n <= 1) return n;
+
+  return fibonacii(n - 1) + fibonacii(n - 2);
 };
 
 console.log(fibonacii(10));

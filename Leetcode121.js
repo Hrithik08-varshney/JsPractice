f6462d4742dbfe1b574a1824d7027e9423591afe
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-07-12 23:27:30 +0530

    leetcode 121 done with greedy algorithm

--- Leetcode121.js
+++ Leetcode121.js
@@ -10,9 +10,23 @@ var maxProfit = function (prices) {
       }
     }
   }
-  return maxProfitVal
+  return maxProfitVal;
 };
 
 // console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-console.log(maxProfit([7,6,4,3,1]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
 
+const maxProfitGreedy = (prices) => {
+  let minStockPrice = prices[0];
+  let profit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minStockPrice) {
+      minStockPrice = prices[i];
+    }
+    profit = Math.max(profit, prices[i] - minStockPrice);
+  }
+  return profit;
+};
+
+console.log(maxProfitGreedy([7, 1, 5, 3, 6, 4]));

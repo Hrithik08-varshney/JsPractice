043f567cb76b8fccc82de0e6303da1628e18047b
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-08-14 23:03:10 +0530

    to add an element to last added

--- LinkedList.js
+++ LinkedList.js
@@ -21,4 +21,22 @@ class LinkedList {
     this.head = newNode;
   }
 
+  
+  //to add an element to last
+
+  addLast(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
 }
